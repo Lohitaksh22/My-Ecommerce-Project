b68a3928af354960c515d588f5785bf5ee4e2528
Lohit <lohitaksh.parvathareddy22@gmail.com>
2025-11-19 17:16:54 -0700

    api

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL ,
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:5173",
   withCredentials: true,
 });
